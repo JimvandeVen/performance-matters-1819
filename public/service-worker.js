dde9ce1805abe975481c6014e529f2175791669a
JimvandeVen <jimvandeven_1@hotmail.com>
2019-03-26 16:36:15 +0100

    made the service-worker save some files in the cache. then is serves them

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,11 +1,29 @@
-self.addEventListener("install", event => {
-    console.log("install")
+self.addEventListener('install', function (e) {
+    console.log(e)
+    e.waitUntil(
+        caches.open('MTG').then(function (cache) {
+            return cache.addAll([
+                '/',
+                '/css/styles-dfe9fb0f18.css',
+                '/js/script-74edf1791a.js',
+                '/images/logo.jpg',
+                '/images/Mtg-logo-700x560.png',
+            ]);
+        })
+    );
 });
 
 self.addEventListener("activate", event => {
     console.log("activate")
 });
 
-self.addEventListener("fetch", event => {
-    console.log("fetch")
+self.addEventListener('fetch', function (event) {
+    console.log(event.request.url);
+
+    event.respondWith(
+        caches.match(event.request)
+            .then(function (response) {
+                return response || fetch(event.request);
+            })
+    );
 });
